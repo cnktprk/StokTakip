784966140da82ec772c446829da6c84ebcd260a8
Nizamettin <Nizamettin@DESKTOP-M7MLD8Q>
2021-02-05 19:51:11 +0300

    ASP.Net Stok Takip Sistemi

--- StokTakip/Stokguncelle.aspx.cs
+++ StokTakip/Stokguncelle.aspx.cs
@@ -8,20 +8,28 @@ using System.Web.UI.WebControls;
 
 namespace StokTakip
 {
-    public partial class Stoklistesi : System.Web.UI.Page
+    public partial class Stokguncelle : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection baglanti = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; initial catalog = StokVeriTabani; integrated security = true;");
-            SqlCommand komut = new SqlCommand("SELECT * FROM StokListesi", baglanti);
-            baglanti.Open();
-            SqlDataReader reader = komut.ExecuteReader();
-            BulGrid.DataSource = reader;
-            BulGrid.DataBind();
-            baglanti.Close();
-            komut.Dispose();
-            baglanti.Close();
+
         }
+
+        protected void UrunGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection(@"Data Source = (localdb)\MSSQLLocalDB; initial catalog = StokVeriTabani; integrated security = true;");
+            SqlCommand komut = new SqlCommand("UPDATE StokListesi SET urunMarka = @urunMarka, urunAdi =  @urunAdi, urunFiyat = @urunFiyat, stokAdet = @stokAdet WHERE barkodNo = @barkodNo", baglanti);
+            komut.Parameters.AddWithValue("@barkodNo", gunUrunBarkod.Text);
+            komut.Parameters.AddWithValue("@urunMarka", gunUrunMarka.Text);
+            komut.Parameters.AddWithValue("@urunAdi", gunUrunAd.Text);
+            komut.Parameters.AddWithValue("@urunFiyat", gunUrunFiyat.Text);
+            komut.Parameters.AddWithValue("@stokAdet", gunStokAdet.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            Response.Redirect("Stoklistesi.aspx");
+        }
+
         protected void UrunBul_Click(object sender, EventArgs e)
         {
             string urunAd = BulUrunAd.Text;
--- StokTakip/Stoksil.aspx.designer.cs
+++ StokTakip/Stoksil.aspx.designer.cs
@@ -11,7 +11,7 @@ namespace StokTakip
 {
 
 
-    public partial class Stoklistesi
+    public partial class Stoksil
     {
 
         /// <summary>
@@ -49,5 +49,23 @@ namespace StokTakip
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource;
+
+        /// <summary>
+        /// gunUrunBarkod control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox gunUrunBarkod;
+
+        /// <summary>
+        /// UrunSil control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button UrunSil;
     }
 }
